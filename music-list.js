700f974dd2d96c20a8af95b6517f40e5609ffcb6
Eman00Mohamed <162371136+Eman00Mohamed@users.noreply.github.com>
2025-04-05 00:34:37 +0200

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+    {
+        name: "El Watar El Hassas",
+        artist: "Sherine",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Kadaba",
+        artist: "Karim Osama",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Lama Bentgahl",
+        artist: "Assala Nasari",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Dehket",
+        artist: "Amr Diab",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "We El Nabi Law Gane",
+        artist: "Sherine",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "هو انت مين",
+        artist: "ِAngham",
+        img: "music-6",
+        src: "music-6"
+    },
+    
+    ];
